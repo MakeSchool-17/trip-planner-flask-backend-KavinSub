d7b088f9f6354323e50d6d2370bcfee248654f41
Kavin Subramanyam <kavin.subramanyam@students.makeschool.com>
2015-10-21 18:17:28 -0700

    Building a Server with Flask completed

--- server.py
+++ server.py
@@ -33,8 +33,92 @@ class MyObject(Resource):
       else:
         return myobject
 
+class Trip(Resource):
+
+    # Create trip
+    def post(self):
+        new_trip = request.json
+        trip_collection = app.db.trips
+
+        result_id = trip_collection.insert_one(new_trip).inserted_id
+
+        trip = trip_collection.find_one({'_id': ObjectId(result_id)})
+
+        return trip
+
+    # Update trip
+    def put(self, trip_id):
+        new_trip = request.json
+        trip_collection = app.db.trips
+
+        trip_collection.update_one({'_id': ObjectId(trip_id)}, {'$set': {'waypoints': new_trip['waypoints']}})
+
+        return trip_collection.find_one({'_id': ObjectId(trip_id)})
+
+    # Delete trip
+    def delete(self, trip_id):
+        trip_collection = app.db.trips
+
+        delete_result = trip_collection.delete_one({'_id': ObjectId(trip_id)})
+
+        return delete_result.raw_result
+
+    # Get trip by id
+    def get(self, trip_id):
+
+        trip_collection = app.db.trips
+        trip = trip_collection.find_one({'_id': ObjectId(trip_id)})
+
+        if trip is None:
+            response = jsonify(data = [])
+            response.status_code = 404
+            return response
+        else:
+            return trip
+
+class User(Resource):
+
+    def post(self):
+        new_user = request.json
+        user_collection = app.db.users
+
+        user_id = user_collection.insert_one(new_user).inserted_id
+        user = user_collection.find_one({'_id': ObjectId(user_id)})
+
+        return user
+
+    def put(self, name):
+        new_trips = request.json
+        user_collection = app.db.users
+
+        user_collection.update_one({'name': name}, {'$set' : {'trips': new_trips}})
+
+        return user_collection.find_one({'name': name})
+
+    def get(self, name):
+        user_collection = app.db.users
+        trip_collection = app.db.trips
+
+        user = user_collection.find_one({'name': name})
+
+        if user is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            trip_ids = user['trips']
+            trips = []
+            for trip in trip_ids:
+                trips.append(trip_collection.find_one({'_id': ObjectId(trip)}))
+            return trips
+
+
+
+
 # Add REST resource to API
 api.add_resource(MyObject, '/myobject/','/myobject/<string:myobject_id>')
+api.add_resource(Trip, '/trips/','/trips/<string:trip_id>')
+api.add_resource(User, '/users/','/users/<string:name>')
 
 # provide a custom JSON serializer for flaks_restful
 @api.representation('application/json')
--- tests.py
+++ tests.py
@@ -21,24 +21,24 @@ class FlaskrTestCase(unittest.TestCase):
     # MyObject tests
 
     def test_posting_myobject(self):
-      response = self.app.post('/myobject/', 
+      response = self.app.post('/myobject/',
         data=json.dumps(dict(
           name="A object"
-        )), 
+        )),
         content_type = 'application/json')
-      
+
       responseJSON = json.loads(response.data.decode())
 
       self.assertEqual(response.status_code, 200)
       assert 'application/json' in response.content_type
       assert 'A object' in responseJSON["name"]
 
-  
+
     def test_getting_object(self):
-      response = self.app.post('/myobject/', 
+      response = self.app.post('/myobject/',
         data=json.dumps(dict(
           name="Another object"
-        )), 
+        )),
         content_type = 'application/json')
 
       postResponseJSON = json.loads(response.data.decode())
@@ -53,6 +53,96 @@ class FlaskrTestCase(unittest.TestCase):
     def test_getting_non_existent_object(self):
       response = self.app.get('/myobject/55f0cbb4236f44b7f0e3cb23')
       self.assertEqual(response.status_code, 404)
+
+    def test_creating_trip(self):
+        trip = {'user':'Kav', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1),('Eiffel Tower', 13.1, 90.8)]}
+        response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        assert 'Kav' in responseJSON['user']
+
+    def test_updating_trip(self):
+        trip = {'user':'Kav', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1)]}
+        response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        responseJSON = json.loads(response.data.decode())
+
+        responseJSON['waypoints'].append(('Eiffel Tower', 13.1, 90.8))
+        new_response = self.app.put('/trips/'+ responseJSON['_id'], data = json.dumps(responseJSON), content_type = 'application/json')
+        new_responseJSON = json.loads(new_response.data.decode())
+
+        self.assertEqual(new_response.status_code, 200)
+        assert new_responseJSON['_id'] == responseJSON['_id']
+        assert 'Kav' in responseJSON['user']
+
+    def test_deleting_trip(self):
+        trip = {'user':'Kav', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1)]}
+        response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        trip_id = json.loads(response.data.decode())['_id']
+
+        new_response = self.app.delete('/trips/' + trip_id)
+        new_responseJSON = json.loads(new_response.data.decode())
+
+        self.assertEqual(new_response.status_code, 200)
+
+    def test_getting_trip(self):
+        trip = {'user':'Kav', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1)]}
+
+        response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        responseJSON = json.loads(response.data.decode())
+        trip_id = responseJSON['_id']
+
+        new_response = self.app.get('/trips/' + trip_id)
+        new_responseJSON = json.loads(new_response.data.decode())
+
+        self.assertEqual(new_response.status_code, 200)
+        assert 'Kav' in responseJSON['user']
+
+    def test_creating_user(self):
+        user = {'name': 'Kav', 'pass': '1@!!!', 'trips':[]}
+
+        response = self.app.post('/users/', data = json.dumps(user), content_type = 'application/json')
+        responseJSON = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 200)
+        assert 'Kav' in responseJSON['name']
+        assert '1@!!!' in responseJSON['pass']
+
+    def test_updating_user(self):
+        user = {'name': 'Kav', 'pass': '1@!!!', 'trips':[]}
+        trip = {'user':'Kav', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1)]}
+
+        # Create user
+        user_response = self.app.post('/users/', data = json.dumps(user), content_type = 'application/json')
+        user_responseJSON = json.loads(user_response.data.decode())
+
+        # Create trip
+        trip_response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        trip_responseJSON = json.loads(trip_response.data.decode())
+
+        # Update user
+        user['trips'].append(trip_responseJSON['_id'])
+        new_response = self.app.put('/users/' + user['name'], data = json.dumps(user['trips']), content_type = 'application/json')
+        new_responseJSON = json.loads(new_response.data.decode())
+
+        # Check
+        self.assertEqual(new_response.status_code, 200)
+        assert 'Kav' in new_responseJSON['name']
+        assert trip_responseJSON['_id'] == new_responseJSON['trips'][0]
+
+    def test_getting_usertrips(self):
+        user = {'name': 'Kav', 'pass': '1@!!!', 'trips':[]}
+        trip = {'name': 'SF trip', 'waypoints':[('Golden Gate Bridge', 23.57, 40.1)]}
+
+        trip_response = self.app.post('/trips/', data = json.dumps(trip), content_type = 'application/json')
+        trip_responseJSON = json.loads(trip_response.data.decode())
+
+        user['trips'].append(trip_responseJSON['_id'])
+        self.app.post('/users/', data = json.dumps(user), content_type = 'application/json')
+
+        get_response = self.app.get('/users/' + user['name'])
+        trips = json.loads(get_response.data.decode())
+
+        self.assertEqual(get_response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
